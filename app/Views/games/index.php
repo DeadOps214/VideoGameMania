157e308a8f38d764ea385002b6c4e379aa949c85
Lewis.G <148462312+DeadOps214@users.noreply.github.com>
2025-03-19 16:30:50 +0000

    Website now can display images taken from a url within the database

--- app/Views/games/index.php
+++ app/Views/games/index.php
@@ -3,11 +3,15 @@
     <?php foreach ($games_name as $games_item): ?>
 
         <h3><?= esc($games_item['game_name']) ?></h3>
-
         <div class="main">
+			<p><img src="<?php echo esc($games_item['Image_URL']) ?>" ></p>
             <?= esc($games_item['genre']) ?>
         </div>
-
+	<div class="container-sm">100% wide until small breakpoint</div>
+	<div class="container-md">100% wide until medium breakpoint</div>
+	<div class="container-lg">100% wide until large breakpoint</div>
+	<div class="container-xl">100% wide until extra large breakpoint</div>
+	<div class="container-xxl">100% wide until extra extra large breakpoint</div>
 
     <?php endforeach ?>
 
